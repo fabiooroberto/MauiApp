5bd3a68c2d6c225bcc8043387bb5cd15ad3ec727
Fábio Roberto <fabioo.roberto@outlook.com>
2023-12-30 22:45:06 -0300

    Add project files.

---
+++ Anotacoes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Anotacoes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Anotacoes/Models/Sobre.cs
@@ -0,0 +1,10 @@
+﻿namespace Anotacoes.Models
+{
+    internal class Sobre
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Este app é escirto em XAML e C# com .NET MAUI.";
+    }
+}
